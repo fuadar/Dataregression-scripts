16445c72cfe05a2f23f078eb8e402ea04bbd8a1a
Fuad Arshad <fuadar@yahoo.com>
2019-06-29 22:03:48 -0500

    Update example_practice_pca_subset.R
    change to fix variables

--- example_practice_pca_subset.R
+++ example_practice_pca_subset.R
@@ -12,8 +12,8 @@ Grilches <- read.csv("Griliches.csv") # read data from the file in the working d
 gril_subset <- Grilches[,c("iq" , "age80", "school80", "expr80", "tenure80")] # select only these variables for 
 # further analysis 
 
-pca_wages <- prcomp(gril_subset, center = TRUE, scale. = TRUE) 
-summary(pca_wages)# output of PCA model
+pca_grils <- prcomp(gril_subset, center = TRUE, scale. = TRUE) 
+summary(pca_grils)# output of PCA model
 
 # Proportion of Variance explained by 5 PCs 0.3969 0.2760 0.1495 0.1184 0.05907
 
