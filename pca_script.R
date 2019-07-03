8201d253a7eaeefc1919be74f654efc7f6d7ce15
Fuad Arshad <fuadar@yahoo.com>
2019-07-03 11:24:40 -0500

    Update pca_script.R

--- pca_script.R
+++ pca_script.R
@@ -3,4 +3,6 @@
 setwd("/Users/farshad/Documents/github/Dataregression-scripts") # this allows you to set a folder of your choice 
 
 pcs <- pc$x
+pcrot <- pc$rot
 write.csv(x=pcs, file="principal_components.csv", row.names=FALSE)
+write.csv(x=pcrot, file="principal_components_rot.csv")
